2f088da471d90c49ad1b28d01d3ca323553eecbe
IanSSenne <IanSSenne@gmail.com>
2020-05-12 00:19:06 -0700

    hi

---
+++ WALLS.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="walls" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="walls.png" width="96" height="96"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="8"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain=",0,,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,,0,"/>
+ <tile id="6" terrain=",0,,"/>
+ <tile id="7" terrain="0,0,,"/>
+ <tile id="8" terrain="0,,,"/>
+</tileset>
